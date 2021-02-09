20bbbbf771fe0e5f129f242881fc2ff8a33a0290
lycho33 <cho.lyds@gmail.com>
2021-02-08 21:55:46 -0500

    commit tree 2ff905a8387da7da6b85a3083e51759e66d245f7

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+    if name_hash.length == 0
+        return nil
+    else 
+        nameWithSmallestValue = []
+        name_hash.each { |name, num| nameWithSmallestValue << num}     
+        min_number = nameWithSmallestValue[0]
+        nameWithSmallestValue.each { |num| min_number = num if num < min_number}
+        name_hash.detect { |name, num| 
+            if min_number == num
+                return name
+            end}
+    end
 end
